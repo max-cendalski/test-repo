8976ac6e320111c137779dc748628f34b72bd2ea
Korneliu <olbiton@gmail.com>
2021-11-29 10:20:07 -0800

    added array with ships

--- js/data.js
+++ js/data.js
@@ -1,1 +1,23 @@
 /* exported data */
+
+var ships = [
+  {
+    brand: 'MISC',
+    length: 42,
+    height: 8,
+    crew: 3
+  },
+  {
+    brand: 'Anvil',
+    length: 60,
+    height: 12,
+    crew: 6
+  },
+  {
+    brand: 'Drake',
+    length: 12,
+    height: 4,
+    crew: 2
+  }
+
+]
